e197641eb45a351795e1b749e0f2fbf0437d4745
Héctor Luis Rivero <hectorluisrivero14@gmail.com>
2023-09-06 20:04:57 -0300

    ultimos retoques

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,9 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store'
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://backendpokemon-production-f1f6.up.railway.app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
