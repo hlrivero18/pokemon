a0dddc28593cc8811e774a1d98547f2001839ff8
Héctor Luis Rivero <79018226+hlrivero18@users.noreply.github.com>
2024-01-02 18:57:04 -0300

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,7 @@ import { Provider } from 'react-redux';
 import store from './redux/store'
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://backendpokemon-production-f1f6.up.railway.app';
+axios.defaults.baseURL = 'https://backendpokemon-production-fb6e.up.railway.app/';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
