1b0290a967f961f07ae793b201c4bf8a39577f63
Héctor Luis Rivero <79018226+hlrivero18@users.noreply.github.com>
2024-08-27 23:44:58 -0300

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,7 @@ import { Provider } from 'react-redux';
 import store from './redux/store'
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://backendpokemon-p9eh.onrender.com/';
+axios.defaults.baseURL = 'https://backendpokemon-1.onrender.com/';
 // axios.defaults.baseURL = 'http://localhost:3001/';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
