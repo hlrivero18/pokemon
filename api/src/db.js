1d60e92c7c4f4d668291286b9c4052e05816f685
hrivero <hrivero@kaizen2b.com>
2025-08-31 15:12:00 -0300

    Optimizando ambiente de produccion

--- api/src/db.js
+++ api/src/db.js
@@ -2,22 +2,35 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_DEPLOY } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
 
-const sequelize = new Sequelize(
-   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/pokemon`,
-   {
-      logging: false, // set to console.log to see the raw SQL queries
-      native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-   }
-);
-
-// const sequelize = new Sequelize(DB_DEPLOY,
+// const sequelize = new Sequelize(
+//    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/pokemon`,
 //    {
 //       logging: false, // set to console.log to see the raw SQL queries
 //       native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 //    }
 // );
+
+const sequelize = new Sequelize(
+   DB_NAME, 
+   DB_USER,
+   DB_PASSWORD,
+   {
+      host: DB_HOST,
+      port: DB_PORT || 5432,
+      dialect: 'postgres',
+      logging: false,
+      dialectOptions:{
+         ssl: {
+            require: true,
+            rejectUnauthorized: false,
+         },
+      },
+   }
+);
+
+
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
